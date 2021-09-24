9c118164194230f32befd5b6d9d4ca2744d539e9
apoloGG <rozilaynen@gmail.com>
2021-09-24 14:57:01 +0300

    add validation user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :email, :name, presence: true
+  validates :email,  uniqueness: true, on: [:create, :update]
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}, on: [:create, :update]
 end
