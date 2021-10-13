f09f22817fa40d5786b5364acbf3accbab113c4d
apoloGG <rozilaynen@gmail.com>
2021-10-13 17:58:10 +0300

    implement user avatars && add AWS cloud

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,13 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      aws_access_key_id:     'AKIATO26RYFMPRLDGLOY',
+      aws_secret_access_key: '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'
+      # aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      # aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    # config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = 'massovka-bucket'
   end
 end
