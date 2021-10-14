9e846a72e16285169f0207d61f6f34026a2c471b
apoloGG <rozilaynen@gmail.com>
2021-10-14 17:14:13 +0300

    Revert "device sign_up add parameters"
    This reverts commit 38c3f402982c917f7a67b8f64bff071142e54599.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,13 @@ class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
  helper_method :current_user_can_edit?
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys:[:password, :password_confirmation, :current_password])
+  end
+ 
   def current_user_can_edit?(model)
     user_signed_in? && 
       (model.user == current_user || model.try(:event).present? && model.event.user == current_user)
   end
-
-  protected
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:account_update, keys:[:password, :password_confirmation, :current_password])
-      devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email, :password])
-    end
 
 end
