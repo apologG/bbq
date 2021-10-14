294304597466af03e803aa4df39d76b42017f925
apoloGG <rozilaynen@gmail.com>
2021-10-14 14:33:37 +0300

    carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,10 @@
-require 'carrierwave/storage/fog'
-
 if Rails.env.production?
   CarrierWave.configure do |config|
-    # config.fog_provider = 'fog/aws'
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     ENV['S3_ACCESS_KEY'],
       aws_secret_access_key: ENV['S3_SECRET_KEY'],
-      use_iam_profile:       false,
       region:                'eu-north-1'
     }
 
