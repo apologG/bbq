634383e0ef9406cdc2e997619a7d8ca3a9c26bc7
apoloGG <rozilaynen@gmail.com>
2021-10-14 12:42:14 +0300

    carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,13 +5,15 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     'AKIATO26RYFMPRLDGLOY',
-      aws_secret_access_key: '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'
-      # aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      # aws_secret_access_key: ENV['S3_SECRET_KEY']
+      # aws_access_key_id:     'AKIATO26RYFMPRLDGLOY',
+      # aws_secret_access_key: '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'
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY']
     }
 
-    # config.fog_directory  = ENV['S3_BUCKET_NAME']
-    config.fog_directory  = 'massovka-bucket'
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_public = false
+    config.aws_acl = :private
+    # config.fog_directory  = 'massovka-bucket'
   end
 end
